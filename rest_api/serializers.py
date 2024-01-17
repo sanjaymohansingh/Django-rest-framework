aa4fe7bd14c761458e19756458af93f5e68f2c25
sanjaymohansingh <sanjaysingh19p@gmail.com>
2024-01-17 07:11:51 -0800

    using model serializers

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 from .models import Post 
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(default='')
+# using serializers
+# class PostSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+#     author = serializers.CharField(max_length=100)
+#     email = serializers.EmailField(default='')
 
-    def create(self, validated_data):
-        return Post.objects.create(validated_data)
+#     def create(self, validated_data):
+#         return Post.objects.create(validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.email = validated_data.get('email', instance.email)
        
+# using serializers.ModelSerializer
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'author', 'email']
