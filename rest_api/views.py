391386c153b62722b84ca3ee0f081111bbf99962
sanjaymohansingh <sanjaysingh19p@gmail.com>
2024-01-17 07:25:37 -0800

    updateing view

--- rest_api/views.py
+++ rest_api/views.py
@@ -6,7 +6,7 @@ from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
-def Posts(request):
+def PostsView(request):
     if request.method == 'GET':
         posts = Post.objects.all() #querySet
         serializer = PostSerializer(posts, many=True)
@@ -15,7 +15,7 @@ def Posts(request):
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = PostSerializer(data=data)
-        
+
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
