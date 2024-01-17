09a10703c1c1a6b396806215c7d877f0161eb217
sanjaymohansingh <sanjaysingh19p@gmail.com>
2024-01-17 07:37:33 -0800

    addin csrf_exempt

--- rest_api/views.py
+++ rest_api/views.py
@@ -3,9 +3,11 @@ from .models import Post
 from .serializers import PostSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+csrf_exempt
 def PostsView(request):
     if request.method == 'GET':
         posts = Post.objects.all() #querySet
