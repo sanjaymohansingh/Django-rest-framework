7baf9f72ea3c39bb60ece5a18767cc00878f853a
sanjaymohansingh <sanjaysingh19p@gmail.com>
2024-01-18 04:45:37 -0800

    using apiView for curd operation

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,47 +1,94 @@
 from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
-from django.http import JsonResponse, HttpResponse
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+
+# using normal views
+# from django.http import JsonResponse, HttpResponse
+# from rest_framework.parsers import JSONParser
+# from django.views.decorators.csrf import csrf_exempt
+
+# using APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-@csrf_exempt
+# using normal views
+
+# @csrf_exempt
+# def PostsView(request):
+#     if request.method == 'GET':
+#         posts = Post.objects.all() #querySet
+#         serializer = PostSerializer(posts, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+    
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = PostSerializer(data=data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
+    
+# @csrf_exempt
+# def Posts_detail(request, pk):
+#     try:
+#         post = Post.objects.get(pk=pk)
+#     except Post.DoesNotExist:
+#         return HttpResponse( status=404)
+
+#     if request.method == 'GET':
+#         serializer = PostSerializer(post)
+#         return JsonResponse(serializer.data)
+    
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = PostSerializer(post, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+    
+#     elif request.method == 'DELETE':
+#         post.delete()
+#         return HttpResponse( status=204)
+
+# using APIView
+@api_view(['GET', 'POST'])
 def PostsView(request):
     if request.method == 'GET':
         posts = Post.objects.all() #querySet
         serializer = PostSerializer(posts, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
+        serializer = PostSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def Posts_detail(request, pk):
     try:
-        post = Post.objects.get(pk=pk)
+        post = Post.objects.get(pk=pk)#instance
     except Post.DoesNotExist:
-        return HttpResponse( status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PostSerializer(post)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(post, data=data)
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         post.delete()
-        return HttpResponse( status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
